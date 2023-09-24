caca61eec780de133d2d1280f5378c5ee6c1fb65
AkihroTajiam <32767695+AkihiroTajima@users.noreply.github.com>
2023-09-25 00:46:38 +0900

    reorganize

---
+++ abc312/c.cpp
@@ -0,0 +1,103 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+// #define int long long
+
+template <class T>
+bool INRANGE(T x, T a, T b) { return a <= x && x <= b; }
+template <class T>
+inline bool chmin(T &a, T b) {if (a > b) {a = b; return true; } return false;}
+template <class T>
+inline bool chmax(T &a, T b) {if (a < b) {a = b; return true; } return false;}
+
+template<typename T>
+ostream& operator << (ostream& os, vector<T>& vec) {
+    os << "[";
+    for (int i = 0; i<vec.size(); i++) os << vec[i] << (i + 1 == vec.size() ? "" : ", ");
+    os << "]";
+    return os;
+}
+
+template<typename T, typename U>
+ostream& operator << (ostream& os, pair<T, U>& pair_var) {
+    os << "(" << pair_var.first << ", " << pair_var.second << ")";
+    return os;
+}
+
+template<typename T, typename U>
+ostream& operator << (ostream& os, map<T, U>& map_var) {
+    os << "{";
+    for (typename map<T, U>::iterator itr = map_var.begin(); itr != map_var.end(); itr++) {
+        os << "(" << itr->first << ", " << itr->second << ")";
+        itr++;
+        if(itr != map_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+template<typename T>
+ostream& operator << (ostream& os, set<T>& set_var) {
+    os << "{";
+    for (auto itr = set_var.begin(); itr != set_var.end(); itr++) {
+        os << *itr;
+        ++itr;
+        if(itr != set_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define RREP(i, n) for (int i = (n); i >= 0; --i)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define RFOR(i, a, b) for (int i = (a); i >= (b); --i)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define debug(x) cerr << #x << " = " << (x) << endl;
+
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n, m;
+    cin >> n >> m;
+
+    vi a(n);
+    REP(i, n) cin >> a[i];
+    vi b(m);
+    REP(i, m) cin >> b[i];
+
+    sort(ALL(a));
+    sort(ALL(b));
+
+    int l = 0;
+    int r = 1e9+5;
+
+    auto urite = [&](int x) {
+        auto it = upper_bound(ALL(a), x);
+        int cnt = distance(a.begin(), it);
+        return cnt;
+    };
+    auto kaite = [&](int x) {
+        auto it = lower_bound(ALL(b), x);
+        int cnt = m - distance(b.begin(), it);
+        return cnt;
+    };
+    while (r-l > 1) {
+        int mid = (r+l)/2;
+        if (urite(mid) >= kaite(mid)) r = mid;
+        else l = mid;
+    }
+
+    cout << r << endl;
+    return 0;
+}
