c37f50f4ce7cb12baaa179563a376f07fb1549a6
AkihroTajiam <32767695+AkihiroTajima@users.noreply.github.com>
2023-12-18 01:35:28 +0900

    solve:abc333_d

--- abc333/d.cpp
+++ abc333/d.cpp
@@ -63,12 +63,48 @@ typedef vector<string> vs;
 typedef vector<vi> vvi;
 typedef pair<int, int> pii;
 
+// UnionFind
+// coding: https://youtu.be/TdR816rqc3s?t=726
+// comment: https://youtu.be/TdR816rqc3s?t=6822
+struct UnionFind {
+  vector<int> d;
+  UnionFind(int n=0): d(n,-1) {}
+  int find(int x) {
+    if (d[x] < 0) return x;
+    return d[x] = find(d[x]);
+  }
+  bool unite(int x, int y) {
+    x = find(x); y = find(y);
+    if (x == y) return false;
+    if (d[x] > d[y]) swap(x,y);
+    d[x] += d[y];
+    d[y] = x;
+    return true;
+  }
+  bool same(int x, int y) { return find(x) == find(y);}
+  int size(int x) { return -d[find(x)];}
+};
+
 int main()
 {
     cin.tie(0);
     ios::sync_with_stdio(false);
 
+    int n;
+    cin >> n;
 
+    UnionFind uf(n);
+    REP(_, n-1) {
+        int u, v;
+        cin >> u >> v;
+        u--, v--;
 
+        if (u!=0 and v!=0) uf.unite(u, v);
+    }
+
+    int max_f = 1;
+    FOR(i, 1, n) max_f = max(max_f, uf.size(i));
+
+    cout << n - max_f << endl;
     return 0;
 }
