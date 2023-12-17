69244e86faea7b85e1080236ed97f04d600a7b5d
AkihroTajiam <32767695+AkihiroTajima@users.noreply.github.com>
2023-12-18 00:31:32 +0900

    solve:abc333_c

---
+++ b.cpp
@@ -0,0 +1,87 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+// #define int long long
+
+template <class T>
+bool INRANGE(T x, T a, T b) { return a <= x && x <= b; }
+template <class T>
+inline bool chmin(T &a, T b) {if (a > b) {a = b; return true; } return false;}
+template <class T>
+inline bool chmax(T &a, T b) {if (a < b) {a = b; return true; } return false;}
+
+template<typename T>
+ostream& operator << (ostream& os, vector<T>& vec) {
+    os << "[";
+    for (int i = 0; i<vec.size(); i++) os << vec[i] << (i + 1 == vec.size() ? "" : ", ");
+    os << "]";
+    return os;
+}
+
+template<typename T, typename U>
+ostream& operator << (ostream& os, pair<T, U>& pair_var) {
+    os << "(" << pair_var.first << ", " << pair_var.second << ")";
+    return os;
+}
+
+template<typename T, typename U>
+ostream& operator << (ostream& os, map<T, U>& map_var) {
+    os << "{";
+    for (typename map<T, U>::iterator itr = map_var.begin(); itr != map_var.end(); itr++) {
+        os << "(" << itr->first << ", " << itr->second << ")";
+        itr++;
+        if(itr != map_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+template<typename T>
+ostream& operator << (ostream& os, set<T>& set_var) {
+    os << "{";
+    for (auto itr = set_var.begin(); itr != set_var.end(); itr++) {
+        os << *itr;
+        ++itr;
+        if(itr != set_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define RREP(i, n) for (int i = (n); i >= 0; --i)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define RFOR(i, a, b) for (int i = (a); i >= (b); --i)
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define debug(x) cerr << #x << " = " << (x) << endl;
+
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    string s, t;
+    cin >> s >> t;
+
+    auto is_next = [](string str) {
+        char c1 = str[0];
+        char c2 = str[1];
+        if (c1 > c2) swap(c1, c2);
+
+        if (c2-c1 == 1) return true;
+        if (c1=='A' and c2=='E') return true;
+        return false;
+    };
+
+    if (is_next(s) == is_next(t)) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
