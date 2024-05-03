5d0ec0abfeaccd32a8b9fc5a33edfd1ea8f86934
AkihroTajiam <32767695+AkihiroTajima@users.noreply.github.com>
2024-05-03 23:04:49 +0900

    Update and rename util_cmd.txt to refresh.sh

---
+++ refresh.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# oj test -c ./a.out
+
+rm -r test
+
+rm a.cpp
+rm b.cpp
+rm c.cpp
+rm d.cpp
+rm e.cpp
+rm f.cpp
+cp t.cpp a.cpp
+cp t.cpp b.cpp
+cp t.cpp c.cpp
+cp t.cpp d.cpp
+cp t.cpp e.cpp
+cp t.cpp f.cpp
